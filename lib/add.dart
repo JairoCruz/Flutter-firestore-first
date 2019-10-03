a58c56b1d122a0554787809fbf739f40e9288cff
Jairo Cruz <azaveliz@gmail.com>
2019-10-03 14:55:59 -0600

    add validate form input, add group view widget, example

--- lib/add.dart
+++ lib/add.dart
@@ -20,6 +20,9 @@ class MyCustomFrom extends StatefulWidget {
 class _MyCustomFormState extends State<MyCustomFrom> {
   final myController = TextEditingController();
 
+  final _formKey = GlobalKey<FormState>();
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+
   @override
   void dispose() {
     myController.dispose();
@@ -29,27 +32,44 @@ class _MyCustomFormState extends State<MyCustomFrom> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         title: Text("Add Name Baby"),
       ),
-      body: Container(
-        margin: EdgeInsets.all(16.0),
-        child: Center(
-          child: Column(
-            // mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.only(bottom: 14.0),
-                child: TextField(
-                  // obscureText: true, esto es para formato de password
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Add name baby',
+      body: Form(
+        key: _formKey,
+        child: Container(
+          margin: EdgeInsets.all(16.0),
+          child: Center(
+            child: Column(
+              // mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(bottom: 14.0),
+                 /* child: TextFormField(
+                    validator: (value) {
+                      if(value.isEmpty){
+                        return 'Please enter some text';
+                      }
+                      return null;
+                    },
+                  ),*/
+                  child: TextFormField(
+                    // obscureText: true, esto es para formato de password
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Add name baby',
+                    ),
+                    controller: myController,
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter some text';
+                      }
+                      return null;
+                    },
                   ),
-                  controller: myController,
                 ),
-              ),
-             /* RaisedButton(
+                /* RaisedButton(
                 onPressed: () {
                   Firestore.instance
                       .collection('baby')
@@ -64,23 +84,38 @@ class _MyCustomFormState extends State<MyCustomFrom> {
                 ),
                 color: Colors.red,
               ),*/
-              MaterialButton(
-                child: Text("Add",/* style: TextStyle(color: Colors.white),*/),
-                textColor: Colors.white,
-                padding: EdgeInsets.symmetric(horizontal: 60),
-                onPressed: () {
-                  Firestore.instance
-                      .collection('baby')
-                      .document(myController.text.toUpperCase())
-                      .setData({'name': myController.text, 'votes': 10});
-                  print(myController.text);
-                  Navigator.pop(context);
-                },
-                color: Colors.indigoAccent,
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                minWidth: double.infinity,
-              ),
-            ],
+                MaterialButton(
+                  child: Text(
+                    "Add", /* style: TextStyle(color: Colors.white),*/
+                  ),
+                  textColor: Colors.white,
+                  padding: EdgeInsets.symmetric(horizontal: 60),
+                  onPressed: () {
+                  if (_formKey.currentState.validate()){
+
+                    Firestore.instance
+                        .collection('baby')
+                        .document(myController.text.toUpperCase())
+                        .setData({
+                      'name': myController.text,
+                      'votes': 10,
+                      'date': DateTime.now(),
+                    });
+                    print(myController.text);
+                   
+                   // Navigator.pop(context);
+                    
+                    _scaffoldKey.currentState.showSnackBar(SnackBar(content: Text('Processs'),));
+                  
+                  }
+                  },
+                  color: Colors.indigoAccent,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  minWidth: double.infinity,
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:first_flutter_firestore/add.dart';
 import 'package:flutter/material.dart';
+//import 'package:grouped_listview/grouped_listview.dart';
 import 'package:intl/intl.dart';
 
 void main() => runApp(MyApp());
@@ -59,10 +60,11 @@ Widget _buildBody(BuildContext context) {
       if (!snapshot.hasData) return LinearProgressIndicator();
       if (snapshot.data.documents.length == 0)
         return Center(
-          child: Text('No hay datos.', style: TextStyle(
-            //fontWeight: FontWeight.bold,
-            fontSize: 18,
-          )),
+          child: Text('No hay datos.',
+              style: TextStyle(
+                //fontWeight: FontWeight.bold,
+                fontSize: 18,
+              )),
         );
 
       return _buildList(context, snapshot.data.documents);
@@ -75,11 +77,74 @@ Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
     padding: const EdgeInsets.only(top: 20.0),
     children: snapshot.map((data) => _buildListItem(context, data)).toList(),
   );
+
+  /* Este codigo me permite crear una coleccion de Registros para pasarlos al widget GrouptListView
+  var listaRecord = List<Record>();
+  snapshot.forEach((data) {
+    final record = Record.fromSnapshot(data);
+    listaRecord.add(record);
+  });
+
+ */
+
+  /* return GroupedListView<Record, String>(
+    collection: listaRecord,
+    groupBy: (Record r) => DateFormat("d-MMMM-yyyy").format(r.dates),
+    //listBuilder: (BuildContext context, Record r) => ListTile(title: Text(r.name.toString()),),
+    listBuilder: (BuildContext context, Record record) {
+      return Dismissible(
+        key: Key(record.name),
+        direction: DismissDirection.endToStart,
+        onDismissed: (direction) {
+          Firestore.instance
+              .collection("baby")
+              .document(record.name.toUpperCase())
+              .delete();
+          print(record.name);
+        },
+        background: Container(
+          alignment: Alignment.centerRight,
+          padding: EdgeInsets.only(right: 20.0),
+          color: Colors.red,
+          child: Icon(
+            Icons.delete,
+            color: Colors.white,
+          ),
+        ),
+        child: Padding(
+          key: ValueKey(record.name),
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.grey),
+              borderRadius: BorderRadius.circular(5.0),
+            ),
+            child: ListTile(
+              title: Text(record.name),
+              trailing: Text(record.votes.toString()),
+              subtitle:
+                  Text(new DateFormat("d MMMM yyyy").format(record.dates)),
+              onTap: () =>
+                  record.reference.updateData({'votes': record.votes + 1}),
+              /* *no work for me **onTap: () => Firestore.instance.runTransaction((transaction) async {
+               final freshSnapshot = await transaction.get(record.reference);
+               final fresh = Record.fromSnapshot(freshSnapshot);
+
+               await transaction
+                   .update(record.reference, {'votes': fresh.votes + 1});
+             }),*/
+            ),
+          ),
+        ),
+      );
+    },
+    groupBuilder: (BuildContext context, String name) => Text(name),
+  );  */
 }
 
 Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
   final record = Record.fromSnapshot(data);
-
+  //print(data.documentID);
   return Dismissible(
     key: Key(record.name),
     direction: DismissDirection.endToStart,
